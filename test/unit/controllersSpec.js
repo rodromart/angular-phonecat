741f57c2c94d64d90da8eb0440e1dfd90d294268
Rodrigo <rodromart@gmail.com>
2014-08-21 23:43:11 +0200

    step 4

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,23 +1,22 @@
-'use strict';
+describe('PhoneCat controllers', function() {
 
-/* jasmine specs for controllers go here */
-
-describe('PhoneListCtrl', function() {
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
     beforeEach(module('phonecatApp'));
 
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope={},
-        ctrl = $controller( 'PhoneListCtrl', {$scope:scope});
-        
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
+
+    it('should create "phones" model with 3 phones', function() {
       expect(scope.phones.length).toBe(3);
-  }));
-    
-it('should return wordl', inject(function($controller){
-    var scope={},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
-    
-    expect(scope.name).toBe('World');
-  }));
-    
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
 });
